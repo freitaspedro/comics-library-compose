d913b8808698bce80dcb9c7eb1aa68e743019c24
freitaspedro <pfreitas@id.uff.br>
2023-08-10 14:55:25 -0300

    add bottom navigation

--- app/src/main/java/com/example/comicslibrary/MainActivity.kt
+++ app/src/main/java/com/example/comicslibrary/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.comicslibrary.ui.theme.ComicsLibraryTheme
+import com.example.comicslibrary.view.CharactersBottomNav
+import com.example.comicslibrary.view.CollectionScreen
+import com.example.comicslibrary.view.LibraryScreen
+
+sealed class Destination(val route: String) {
+    object Library: Destination("library")
+    object Collection: Destination("collection")
+    object CharacterDetail: Destination("character/{characterId}") {
+        fun createRoute(characterId: Int?) = "character/$characterId"
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +35,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    CharactersScaffold(navController = navController)
                 }
             }
         }
@@ -30,14 +44,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun CharactersScaffold(navController: NavHostController) {
+    val scaffoldState = rememberScaffoldState()
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComicsLibraryTheme {
-        Greeting("Android")
+    Scaffold (
+        scaffoldState = scaffoldState,
+        bottomBar = { CharactersBottomNav(navController = navController)}
+    ) {
+        paddingValues ->
+            NavHost(
+                navController = navController,
+                startDestination = Destination.Library.route,
+                modifier = Modifier.padding(paddingValues)
+            ) {
+                composable(Destination.Library.route) { LibraryScreen() }
+                composable(Destination.Collection.route) { CollectionScreen() }
+                composable(Destination.CharacterDetail.route) { navBackStackEntry ->   }
+            }
     }
+
+
 }
